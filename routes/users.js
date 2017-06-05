6ab0d76677e642c47c3b7d797d1667bd830b1c2d
daniel verman <daniverman@gmail.com>
2017-06-05 10:21:21 +0300

    second update daniel

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,41 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../DButils')
+var db = require('../DButils');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('the forst page of /users');
+  res.send('the first page of /users');
 });
+//need to check
 router.post('/login', function(req, res) {
-    var userName = req.query.userName;
-    var password = req.query.password;
-    db.Select
+    var userName = req.body.userName;
+    var password = req.body.password;
+    var qurey = "SELECT * FROM user_tb WHERE userName ='"+userName + "' AND password ='"+ password + "'";
+    db.Select(qurey)
+    {
+    }
+
 
 });
+
+//need to check
+router.post('/register',function (req , res) {
+    var userName = req.body.userName;
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var password = req.body.password;
+    var address = req.body.address;
+    var city = req.body.city;
+    var country = req.body.country;
+    var phone = req.body.phone;
+    var mail = req.body.mail;
+    var creditCard = req.body.creditCard;
+    var gender = req.body.gender;
+    var lastEntry = "NULL";
+    var query ="INSERT INTO user_tb VALUES ('" + userName + "', '"+ firstName + "', '"+ lastName + "', '"+ password + "', '"+ address + "', '"+ city + "', '"+ country + "', '"+ phone + "', '"+ mail + "', '"+ creditCard + "', '"+ gender + "', '"+ creditCardNumber + "', '"+ lastEntry +  ")";
+    db.Insert(query)
+
+});
+
+
 module.exports = router;
